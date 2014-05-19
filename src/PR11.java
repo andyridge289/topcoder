b31aa5cb8f2e0e9ef02489f33d6cf27e18444159
Andy <andyridge289@gmail.com>
2014-05-19 10:36:47 +0100

    pr11

---
+++ src/PR11.java
@@ -0,0 +1,25 @@
+
+public class PR11 
+{
+
+	public static void main(String[] args)
+	{
+		PR11 p = new PR11();
+	}
+	
+	public PR11()
+	{
+//		int[] numbers = new int[] {
+//			12345,
+//			661232,
+//			52527,
+//			730000000
+//		};
+//		
+//		for(int number : numbers) {
+//			int n = howMany(number);
+//			System.out.println(String.format("%d: %d", number, n));
+//		}
+	}
+	
+}
