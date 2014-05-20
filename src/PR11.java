fc12b85e0fc92e7421ed7584f494e55ed8ec88eb
Andy <andyridge289@gmail.com>
2014-05-20 11:06:09 +0100

    pr11

--- src/PR11.java
+++ src/PR11.java
@@ -9,17 +9,55 @@ public class PR11
 	
 	public PR11()
 	{
-//		int[] numbers = new int[] {
-//			12345,
-//			661232,
-//			52527,
-//			730000000
-//		};
-//		
-//		for(int number : numbers) {
-//			int n = howMany(number);
-//			System.out.println(String.format("%d: %d", number, n));
-//		}
+		int[][] arrivals = new int[][] {
+				{ 3, 3, 9},
+				{182},
+				{ 2, 10, 11},
+				{ 2, 10, 12}
+		};
+		
+		int[][] sequences = new int[][] {
+				{ 2, 15, 14 },
+				{ 11},
+				{ 3, 4, 3},
+				{ 15, 1, 15}
+		};
+		
+		for(int i = 0; i < arrivals.length; i++) {
+			int n = maxWait(arrivals[i], sequences[i]);
+			
+			String arrivalString = "";
+			for(int a : arrivals[i])
+				arrivalString += " " + a;
+			
+			String sequenceString = "";
+			for(int a : sequences[i])
+				sequenceString += " " + a;
+			
+			System.out.println(String.format("{%s }, {%s }: %d", arrivalString, sequenceString, n));
+		}
+	}
+	
+	private int maxWait(int[] arrival, int[] sequence)
+	{
+		int priorLeft = 0;
+		int[] waits = new int[arrival.length];
+		
+		for(int i = 0; i < arrival.length; i++)
+		{
+			int orderWait = priorLeft - arrival[i] > 0 ? priorLeft - arrival[i] : 0;
+			int totalWait = orderWait + sequence[i];
+			
+			priorLeft = arrival[i] + totalWait;
+			waits[i] = orderWait;
+		}
+		
+		int max = waits[0];
+		for(int i = 1; i < waits.length; i++)
+			if(max < waits[i])
+				max = waits[i];
+		
+		return max;
 	}
 	
 }
